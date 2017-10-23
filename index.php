6cb0f6e8c8c98a9cba4bb5d43ee3fbb424f23ef0
nadya16 <nadya97vas@yandex.ru>
2017-10-23 05:16:49 +0400

    Коммит

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title></title>
+	<meta name="title" content="" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
+</head>
+<body>
+<?php require 'bloks/header.php';?>
+</div>
+<?php require 'bloks/header2.php'; ?>
+<!-- основной блок -->
+<div id="wrapper">
+ <div id="middle">
+  <div id="container">
+  <div id="content">
+<!-- между метками AAA и BBB будем писать все наполнение сайта -->
+<!-- AAA -->
+<br>
+<table>
+  <tr>
+<td align="center"><img src="img/zamena-masla-besplatno.png"></td>
+</tr>
+</table>
+<br>
+<br>
+<?php require 'bloks/header3.php'; ?>
+<br>
+<table>
+
+ <td><img src="img/auto1.jpg"></td>
+<td><img src="img/auto2.jpg" ></td>
+<td><img src="img/auto3.jpg"></td>
+<td><img src="img/auto4.jpg"></td>
+</tr>
+</table>
+<br>
+<p class="p_h">
+Ваш автосервис в Ульяновске -
+сервис "Автосеть Гараж"
+</p>
+<br>
+<p class="p_text" >
+«Автосеть Гараж» предлагает полный спектр услуг в плане обслуживания и ремонта автомобилей,
+а также осуществляет продажу запчастей.
+ </p>
+<br>
+<p class="p_h1">Услуги автосервиса"Автосеть Гараж"</p>
+<br>
+
+
+<table>
+  <tr>
+<td ><p class="p_h1">Ремонт автомобилей</p></td><td><p class="p_h1">Автомойка</p></td>
+  </tr>
+<tr>
+<td >
+<p class="p_text">К нам можно обратиться за ремонтом двигателя, трансмиссии, автоэлектрики, ходовой части, кузова, а также заменой стекол, масел, фильтров и т.д.</p>
+</td>
+<td>
+<p class="p_text">Вы можете заказать в нашем автосервисе и услуги по мойке и полировке кузова, химчистке салона.</p>
+</td>
+</tr>
+ <tr>
+<td ><p class="p_h1"><img src="img/remont_avto.jpg"></p></td><td><p class="p_h1"><img src="img/avtomojka.jpg"></p></td>
+  </tr>
+  <tr>
+<td ><p class="p_h1">Шиномонтаж</p></td><td><p class="p_h1">Тонировка</p></td>
+  </tr>
+<tr>
+<td>
+<p class="p_text">Автосервис «Гараж» оказывает все услуги, связанные с заменой и балансировкой шин.</p>
+</td>
+<td>
+<p class="p_text">Мы предлагаем профессиональное тонирование стекол автомобилей высококачественными пленками всех видов.</p>
+</td>
+</tr>
+</table>
+
+
+<p class="p_h1">
+Преимущества услуг нашего автосервиса для ВАЗ и других авто<br><br>
+</p>
+<p class="p_text" >
+Есть несколько причин, почему, сравнивая разные автосервисы с «Автосеть Гараж»,многие автолюбители отдают предпочтение именно нам.<br>
+<p class="p_list" >
+    1.Наши мастера являются специалистами в своем деле. Они выполняют все работы не только быстро,но и максимально профессионально. <br>
+<p class="p_list" >
+     2.Мы практикуем индивидуальный подход к каждому клиенту. За какими бы услугами вы ни решили обратиться в автосервис – на ВАЗ или другие отечественные либо зарубежные автомобили, ­– вы всегда можете рассчитывать на качественное обслуживание.<br>
+<p class="p_list" >
+    3.У нас действует гибкая ценовая политика, предусматривающая скидки для постоянных заказчиков.
+</p></p></p>
+<br>
+<p class="p_text" >
+Все это позволяет нам с уверенностью утверждать, что в следующий раз, когда у вас возникнет необходимость в услугах автосервиса, вы обратитесь именно в «Автосеть Гараж».
+</p>
+<br>
+<p class="p_h1">
+Оптимальные цены на услуги автосервиса и запчасти
+</p>
+<p class="p_text">
+Мы рады отметить, что на все услуги и детали, которые предлагает наш автосервис, цены остаются доступными как для крупных организаций, так и простых автовладельцев.
+</p>
+<p class ="p_h">Ждём Вас! <a href="iframe.php"><input value="Записаться в сервис" type="button"></a>
+</p>
+<!-- BBB -->
+<!-- наполнение сайта - наш контент -->
+  </div><!-- #content-->
+</div><!-- #container-->
+</div>
+</div>
+<div id="wrapper">
+<?php require 'bloks/footer.php'; ?>
+</div>
